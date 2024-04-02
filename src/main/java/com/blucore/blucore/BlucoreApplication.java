f214a51cd48b2fba76eefcde1ebb240b2ad17dfc
guru2086 <guru2086@users.noreply.github.com>
2024-04-01 21:10:34 -0500

    pushing spring boot app

---
+++ src/main/java/com/blucore/blucore/BlucoreApplication.java
@@ -0,0 +1,13 @@
+package com.blucore.blucore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlucoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlucoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blucore/blucore/BlucoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blucore.blucore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlucoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
